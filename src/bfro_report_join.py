79418853d45aa6274d31e436d590f0824b3ea81e
Tim Renner <renner.timothy@gmail.com>
2017-11-12 20:26:54 -0600

    Added geohash column to final dataset, set precision of timestamp to date level.

--- src/bfro_report_join.py
+++ src/bfro_report_join.py
@@ -1,7 +1,16 @@
+""" process_bfro_reports.py
+
+    Joins the report locations to the full text reports and performs light 
+    cleaning: removing invalid latitudes and longitudes, removing timestamps
+    that occur in the future, and sets the timestamp granularity to days. Also
+    adds a geohash column for the location for easy geospatial joins.
+"""
+
 import pandas as pd
 import numpy as np
 import click
 import json
+import pygeohash as pgh
 
 from toolz import pluck
 
@@ -9,7 +18,8 @@ from toolz import pluck
 @click.argument("report_locations_file", type=click.File("r"))
 @click.argument("report_file", type=click.File("r"))
 @click.argument("report_join_file", type=click.File("w"))
-def main(report_locations_file, report_file, report_join_file):
+@click.option("--precision", "-p", type=int, default=10)
+def main(report_locations_file, report_file, report_join_file, precision):
     """ Joins the full text reports with the report locations.
     """
 
@@ -26,9 +36,12 @@ def main(report_locations_file, report_file, report_join_file):
         pd.to_datetime(report_locations.timestamp) <= pd.datetime.now()
 
     report_locations = report_locations.loc[
-        valid_latitudes & valid_longitudes & valid_times,
-        :
+        valid_latitudes & valid_longitudes & valid_times, :
     ]
+
+    # Set the report timestamps to the correct period (date).
+    report_locations.loc[:,'date'] = \
+        pd.to_datetime(report_locations.timestamp).dt.to_period('d')
 
     # Make a generator that parses the json.
     report_dicts = filter(
@@ -88,10 +101,23 @@ def main(report_locations_file, report_file, report_join_file):
             'number_report',
             'number_report_location',
             'classification_report',
-            'classification_report_location'
+            'classification_report_location',
+            'timestamp'
         ],
         axis=1,
         inplace=True
+    )
+
+    # Add the geohash column.
+    reports_joined.loc[:,'geohash'] = [
+        pgh.encode(row.latitude, row.longitude, precision=precision)
+        for _,row in reports_joined.iterrows()
+    ]
+    # A null location should result in a null geohash value.
+    reports_joined.loc[:,'geohash'] = np.where(
+        reports_joined.latitude.isnull(),
+        np.NaN,
+        reports_joined.geohash
     )
 
     reports_joined.to_csv(report_join_file, index=False)
