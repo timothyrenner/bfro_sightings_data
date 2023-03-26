b0eae9c9acc96e893de2fe012c9f360466881343
Tim Renner <renner.timothy@gmail.com>
2023-03-26 11:01:15 -0500

    Implemented finalized model for geocoded reports.

---
+++ bfro_mini_warehouse/models/bfro_reports_geocoded.sql
@@ -0,0 +1,47 @@
+{{
+    config(
+        materialized="external",
+        location="../data_new/processed/bfro_reports_geocoded.csv",
+        format="csv"
+    )
+}}
+
+
+SELECT
+    reports.observed,
+    reports.location_details,
+    reports.county,
+    reports.state,
+    reports.season,
+    geo.title,
+    geo.latitude,
+    geo.longitude,
+    COALESCE(reports.date, STRFTIME(geo.timestamp, '%Y-%m-%d')) AS date,
+    COALESCE(reports.report_number, geo.number) AS number,
+    COALESCE(reports.report_class, geo.classification) AS classification,
+    geo.hexid,
+    weather.temperature_high,
+    weather.temperature_mid,
+    weather.temperature_low,
+    weather.dew_point,
+    weather.humidity,
+    weather.cloud_cover,
+    weather.moon_phase,
+    weather.precip_intensity,
+    weather.precip_probability,
+    weather.precip_type,
+    weather.pressure,
+    weather.summary,
+    weather.conditions,
+    weather.uv_index,
+    weather.visibility,
+    weather.wind_bearing,
+    weather.wind_speed,
+FROM
+    {{ source("local_files", "geocoded_reports") }} AS geo
+FULL OUTER JOIN
+    {{ source("local_files", "bfro_reports") }} AS reports
+    ON reports.report_number = geo.number
+LEFT JOIN
+    {{ ref("weather") }} AS weather
+    ON geo.hexid = weather.hexid AND geo.timestamp = weather.timestamp
